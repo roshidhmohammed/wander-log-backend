97636119264db56d1191c851f3d6930aedfa2cd5
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-03-02 18:06:04 +0530

    preflight issue 3

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(
       origin:process.env.API_URL,
       methods: ["GET", "POST", "PUT", "DELETE"],
        allowedHeaders: "Content-Type, Authorization",
-      credentials: true,
+      // credentials: true,
     }
   )
   );
